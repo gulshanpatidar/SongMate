41b800478b4e2c883c7b55d6e2a5f57ecb8d4ec6
GulshanPatidar <jankarikibaat@gmail.com>
2022-01-29 22:54:35 +0530

    initial commit.

---
+++ app/src/main/java/com/example/songmate/ui/AppNavigation.kt
@@ -0,0 +1,9 @@
+package com.example.songmate
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppNavigation() {
+    Text(text = "Hello world from AppNavigation")
+}
---
+++ app/src/main/java/com/example/songmate/ui/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.songmate.ui.util
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import com.example.songmate.ui.models.BottomNavItem
+
+object Constants {
+
+    val bottomNavItems = listOf<BottomNavItem>(
+//        BottomNavItem(icon = Icons.Default.Home)
+    )
+}
